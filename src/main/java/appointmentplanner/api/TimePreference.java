fd00a4b8dc19f11ddd4c44a08ee4101671d217e4
Bonajo <m.bonajo@fontys.nl>
2023-10-03 15:35:01 +0200

    fix: clear up confusion about time preferences

--- src/main/java/appointmentplanner/api/TimePreference.java
+++ src/main/java/appointmentplanner/api/TimePreference.java
@@ -5,32 +5,40 @@ package appointmentplanner.api;
 
 /**
  * Strategy to find a Time slot to add an Appointment.
- * EARLIEST searches for the EARLIEST time slot from the start of the Day.
- * LATEST searches for the LATEST time slot from the start of the Day.
- * 
+ *
  * @author Richard van den Ham {@code r.vandenham@fontys.nl}
  * @author Pieter van den Hombergh {@code p.vandenhombergh@fontys.nl}
  */
 public enum TimePreference {
     /**
-     * Earliest on a day.
+     * Plan the appointment as early as possible on a day.
+     * This preference can work without a fixed time.
      */
-    EARLIEST, 
+    EARLIEST,
     /**
-     * Latest on a day.
+     * Plan as late as possible on a day.
+     * This preference can work without a fixed time.
      */
     LATEST,
     /**
-     * 
      * In case no time preference is specified, this is the default.
+     * In general cases this will fall back to EARLIEST
      */
     UNSPECIFIED,
     /**
-     * Latest before a given time (the whole appointment should fit before the given time).
+     * Plan as late as possible before a given time.
+     * This preference cannot be used without a fixed time.
+     *
+     * NOTE: this preference behaves differently from other preferences,
+     * as it is the only preference where the appointment should not be planned on the fixed time if possible.
+     * The entire appointment should fit BEFORE the fixed time (so the appointment should end before the fixed time).
      */
     LATEST_BEFORE,
     /**
-     * Earliest after a given time (the appointment can also start at the given time).
+     * Plan as early as possible after a given time.
+     * This preference cannot be used without a fixed time.
+     *
+     * NOTE: if planning is possible at the given time, then that is a valid earliest after.
      */
     EARLIEST_AFTER;
 }
